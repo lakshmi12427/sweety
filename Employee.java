9eb330b96c7874f7fd44460d007e2297672bc824
moulichowdhary <moulichowdhary@gmail.com>
2022-09-01 17:43:59 -0400

    change by sweety

---
+++ Employee.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package chocolate;
+
+/**
+ * @author dell
+ *
+ */
+public class Employee {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello sweety and mouli");
+		
+		System.out.print( 2+2);
+
+	}
+
+}
